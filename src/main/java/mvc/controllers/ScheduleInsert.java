b16d790c1cdccff24e86c0a04a40a40d3e0dda8b
Khyeongu <skidrom1@gmail.com>
2021-04-28 17:48:16 +0900

    scheduleInsert page 생성

--- src/main/java/mvc/controllers/ScheduleInsert.java
+++ src/main/java/mvc/controllers/ScheduleInsert.java
@@ -1,5 +1,16 @@
 package mvc.controllers;
 
-public class ScheduleInsert {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import mvc.fx.AbstractController;
+import mvc.fx.ModelAndView;
+
+public class ScheduleInsert extends AbstractController {
+
+	@Override
+	public ModelAndView handleRquestInternal(HttpServletRequest request, HttpServletResponse response) {
+		return new ModelAndView("/WEB-INF/views/scheduleInsert.jsp");
+	}
+	
 }
